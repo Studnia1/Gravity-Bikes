ea870cf6dac194541c69bd33e1ed7e7a7d0b6fcc
jakub studzinski <studzienj@gmail.com>
2020-05-01 10:23:26 +0200

    Added parallaxa.

--- GravityBikes/ClientApp/src/app/app.module.ts
+++ GravityBikes/ClientApp/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ParallaxScrollModule } from 'ng2-parallaxscroll';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ParallaxScrollModule,
     RouterModule.forRoot([
-    ])
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]

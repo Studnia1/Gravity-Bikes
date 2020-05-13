d8960345d811354ee019abcf926c141b80f59e79
studzienj@gmail.com <studzienj@gmail.com>
2020-05-13 09:31:34 +0100

    Some small changes and added respository pattern

---
+++ GravityBikes/Data/AuthRespository.cs
@@ -0,0 +1,34 @@
+﻿using GravityBikes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GravityBikes.Data
+{
+    public class AuthRespository : IAuthRespository
+    {
+        private readonly DataContext _context;
+        public AuthRespository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public DataContext Context { get; }
+
+        public Task<User> Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExsist(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GravityBikes/Data/IAuthRespository.cs
@@ -0,0 +1,15 @@
+﻿using GravityBikes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GravityBikes.Data
+{
+    public interface IAuthRespository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string email, string password);
+        Task<bool> UserExsist(string email);
+    }
+}

4ba916ff4e20dddb0f8bb2e80842881aa7d59ccc
jakub studzinski <studzienj@gmail.com>
2020-06-02 20:23:02 +0200

    Global error handling

---
+++ GravityBikes/Helpers/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace GravityBikes.Helpers
+{
+    static public class Extensions
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Acces-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Acces-Control-Allow-Origin", "*");
+        }
+    }
+}
--- GravityBikes/Startup.cs
+++ GravityBikes/Startup.cs
@@ -1,7 +1,10 @@
  using GravityBikes.Data;
+using GravityBikes.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
 using System.Text;
 
 namespace GravityBikes
@@ -56,9 +60,21 @@ namespace GravityBikes
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+               // app.UseHsts();
             }
 
             app.UseHttpsRedirection();
